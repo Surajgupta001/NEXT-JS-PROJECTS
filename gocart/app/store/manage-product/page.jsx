290f7da1b8128449090cd0bf8d8f05f451f1578e
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-10-23 16:38:27 +0530

    Deploying On vercel

---
+++ gocart/app/store/manage-product/page.jsx
@@ -0,0 +1,70 @@
+'use client'
+import { useEffect, useState } from "react"
+import { toast } from "react-hot-toast"
+import Image from "next/image"
+import Loading from "@/components/Loading"
+import { productDummyData } from "@/assets/assets"
+
+export default function StoreManageProducts() {
+
+    const currency = process.env.NEXT_PUBLIC_CURRENCY_SYMBOL || '$'
+
+    const [loading, setLoading] = useState(true)
+    const [products, setProducts] = useState([])
+
+    const fetchProducts = async () => {
+        setProducts(productDummyData)
+        setLoading(false)
+    }
+
+    const toggleStock = async (productId) => {
+        // Logic to toggle the stock of a product
+
+
+    }
+
+    useEffect(() => {
+            fetchProducts()
+    }, [])
+
+    if (loading) return <Loading />
+
+    return (
+        <>
+            <h1 className="mb-5 text-2xl text-slate-500">Manage <span className="font-medium text-slate-800">Products</span></h1>
+            <table className="w-full max-w-4xl overflow-hidden text-sm text-left rounded ring ring-slate-200">
+                <thead className="tracking-wider text-gray-700 uppercase bg-slate-50">
+                    <tr>
+                        <th className="px-4 py-3">Name</th>
+                        <th className="hidden px-4 py-3 md:table-cell">Description</th>
+                        <th className="hidden px-4 py-3 md:table-cell">MRP</th>
+                        <th className="px-4 py-3">Price</th>
+                        <th className="px-4 py-3">Actions</th>
+                    </tr>
+                </thead>
+                <tbody className="text-slate-700">
+                    {products.map((product) => (
+                        <tr key={product.id} className="border-t border-gray-200 hover:bg-gray-50">
+                            <td className="px-4 py-3">
+                                <div className="flex items-center gap-2">
+                                    <Image width={40} height={40} className='p-1 rounded shadow cursor-pointer' src={product.images[0]} alt="" />
+                                    {product.name}
+                                </div>
+                            </td>
+                            <td className="hidden max-w-md px-4 py-3 truncate text-slate-600 md:table-cell">{product.description}</td>
+                            <td className="hidden px-4 py-3 md:table-cell">{currency} {product.mrp.toLocaleString('en-US')}</td>
+                            <td className="px-4 py-3">{currency} {product.price.toLocaleString('en-US')}</td>
+                            <td className="px-4 py-3 text-center">
+                                <label className="relative inline-flex items-center gap-3 text-gray-900 cursor-pointer">
+                                    <input type="checkbox" className="sr-only peer" onChange={() => toast.promise(toggleStock(product.id), { loading: "Updating data..." })} checked={product.inStock} />
+                                    <div className="h-5 transition-colors duration-200 rounded-full w-9 bg-slate-300 peer peer-checked:bg-green-600"></div>
+                                    <span className="absolute w-3 h-3 transition-transform duration-200 ease-in-out bg-white rounded-full dot left-1 top-1 peer-checked:translate-x-4"></span>
+                                </label>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    )
+}
---
+++ gocart/app/store/orders/page.jsx
@@ -0,0 +1,159 @@
+'use client'
+import { useEffect, useState } from "react"
+import Loading from "@/components/Loading"
+import { orderDummyData } from "@/assets/assets"
+
+export default function StoreOrders() {
+    const [orders, setOrders] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [selectedOrder, setSelectedOrder] = useState(null)
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+
+    const fetchOrders = async () => {
+       setOrders(orderDummyData)
+       setLoading(false)
+    }
+
+    const updateOrderStatus = async (orderId, status) => {
+        // Logic to update the status of an order
+
+
+    }
+
+    const openModal = (order) => {
+        setSelectedOrder(order)
+        setIsModalOpen(true)
+    }
+
+    const closeModal = () => {
+        setSelectedOrder(null)
+        setIsModalOpen(false)
+    }
+
+    useEffect(() => {
+        fetchOrders()
+    }, [])
+
+    if (loading) return <Loading />
+
+    return (
+        <>
+            <h1 className="mb-5 text-2xl text-slate-500">Store <span className="font-medium text-slate-800">Orders</span></h1>
+            {orders.length === 0 ? (
+                <p>No orders found</p>
+            ) : (
+                <div className="max-w-4xl overflow-x-auto border border-gray-200 rounded-md shadow">
+                    <table className="w-full text-sm text-left text-gray-600">
+                        <thead className="text-xs tracking-wider text-gray-700 uppercase bg-gray-50">
+                            <tr>
+                                {["Sr. No.", "Customer", "Total", "Payment", "Coupon", "Status", "Date"].map((heading, i) => (
+                                    <th key={i} className="px-4 py-3">{heading}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody className="divide-y divide-gray-100">
+                            {orders.map((order, index) => (
+                                <tr
+                                    key={order.id}
+                                    className="transition-colors duration-150 cursor-pointer hover:bg-gray-50"
+                                    onClick={() => openModal(order)}
+                                >
+                                    <td className="pl-6 text-green-600" >
+                                        {index + 1}
+                                    </td>
+                                    <td className="px-4 py-3">{order.user?.name}</td>
+                                    <td className="px-4 py-3 font-medium text-slate-800">${order.total}</td>
+                                    <td className="px-4 py-3">{order.paymentMethod}</td>
+                                    <td className="px-4 py-3">
+                                        {order.isCouponUsed ? (
+                                            <span className="px-2 py-1 text-xs text-green-700 bg-green-100 rounded-full">
+                                                {order.coupon?.code}
+                                            </span>
+                                        ) : (
+                                            "—"
+                                        )}
+                                    </td>
+                                    <td className="px-4 py-3" onClick={(e) => { e.stopPropagation() }}>
+                                        <select
+                                            value={order.status}
+                                            onChange={e => updateOrderStatus(order.id, e.target.value)}
+                                            className="text-sm border-gray-300 rounded-md focus:ring focus:ring-blue-200"
+                                        >
+                                            <option value="ORDER_PLACED">ORDER_PLACED</option>
+                                            <option value="PROCESSING">PROCESSING</option>
+                                            <option value="SHIPPED">SHIPPED</option>
+                                            <option value="DELIVERED">DELIVERED</option>
+                                        </select>
+                                    </td>
+                                    <td className="px-4 py-3 text-gray-500">
+                                        {new Date(order.createdAt).toLocaleString('en-US', { timeZone: 'UTC' })}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
+
+            {/* Modal */}
+            {isModalOpen && selectedOrder && (
+                <div onClick={closeModal} className="fixed inset-0 z-50 flex items-center justify-center text-sm bg-black/50 text-slate-700 backdrop-blur-xs" >
+                    <div onClick={e => e.stopPropagation()} className="relative w-full max-w-2xl p-6 bg-white rounded-lg shadow-lg">
+                        <h2 className="mb-4 text-xl font-semibold text-center text-slate-900">
+                            Order Details
+                        </h2>
+
+                        {/* Customer Details */}
+                        <div className="mb-4">
+                            <h3 className="mb-2 font-semibold">Customer Details</h3>
+                            <p><span className="text-green-700">Name:</span> {selectedOrder.user?.name}</p>
+                            <p><span className="text-green-700">Email:</span> {selectedOrder.user?.email}</p>
+                            <p><span className="text-green-700">Phone:</span> {selectedOrder.address?.phone}</p>
+                            <p><span className="text-green-700">Address:</span> {`${selectedOrder.address?.street}, ${selectedOrder.address?.city}, ${selectedOrder.address?.state}, ${selectedOrder.address?.zip}, ${selectedOrder.address?.country}`}</p>
+                        </div>
+
+                        {/* Products */}
+                        <div className="mb-4">
+                            <h3 className="mb-2 font-semibold">Products</h3>
+                            <div className="space-y-2">
+                                {selectedOrder.orderItems.map((item, i) => (
+                                    <div key={i} className="flex items-center gap-4 p-2 border rounded shadow border-slate-100">
+                                        <img
+                                            src={item.product.images?.[0].src || item.product.images?.[0]}
+                                            alt={item.product?.name}
+                                            className="object-cover w-16 h-16 rounded"
+                                        />
+                                        <div className="flex-1">
+                                            <p className="text-slate-800">{item.product?.name}</p>
+                                            <p>Qty: {item.quantity}</p>
+                                            <p>Price: ${item.price}</p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+
+                        {/* Payment & Status */}
+                        <div className="mb-4">
+                            <p><span className="text-green-700">Payment Method:</span> {selectedOrder.paymentMethod}</p>
+                            <p><span className="text-green-700">Paid:</span> {selectedOrder.isPaid ? "Yes" : "No"}</p>
+                            {selectedOrder.isCouponUsed && (
+                                <p><span className="text-green-700">Coupon:</span> {selectedOrder.coupon.code} ({selectedOrder.coupon.discount}% off)</p>
+                            )}
+                            <p><span className="text-green-700">Status:</span> {selectedOrder.status}</p>
+                            <p><span className="text-green-700">Order Date:</span> {new Date(selectedOrder.createdAt).toLocaleString('en-US', { timeZone: 'UTC' })}</p>
+                        </div>
+
+                        {/* Actions */}
+                        <div className="flex justify-end">
+                            <button onClick={closeModal} className="px-4 py-2 rounded bg-slate-200 hover:bg-slate-300" >
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
