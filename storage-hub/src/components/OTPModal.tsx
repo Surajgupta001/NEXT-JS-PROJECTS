a56acd18d80374e0df430d27a59302cc23bf4a8b
surajgupta001 <surajgupta7070031833@gmail.com>
2026-06-02 18:23:15 +0530

    feat: add new SVG icons for video and upload, and update layout components
    - Added new SVG icons: thumb-video.svg, upload.svg, and video.svg.
    - Introduced new images: avatar.png, files-2.png, files.png, photo.png, hero.png, jsmpro.png, and videokit.png.
    - Created layout component for authentication pages with responsive design.
    - Implemented sign-in and sign-up pages using a reusable AuthForm component.
    - Added utility components for form handling: Form, FormField, FormItem, FormLabel, FormControl, FormMessage.
    - Enhanced global styles with Tailwind CSS and custom variants.
    - Introduced utility functions for class name merging.

---
+++ storage-hub/src/components/OTPModal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OTPModal({ accountId, email } : { accountId: string, email: string }) {
+    return (
+        <div>OTPModal</div>
+    )
+}
+
+export default OTPModal
