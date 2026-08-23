8b140cdb0307ba6218461d90bf89787a2b8edd8b
surajgupta001 <surajgupta7070031833@gmail.com>
2026-08-23 05:41:17 +0530

    feat(ui): build LMS authentication UI and branding - Add responsive login and authentication layout - Add dark theme support with ThemeProvider - Fix ThemeProvider compatibility with Next.js 16 - Add minimal white LMS logo for dark UI

--- lms-course-platform/src/app/(auth)/layout.tsx
+++ lms-course-platform/src/app/(auth)/layout.tsx
@@ -1,10 +1,27 @@
+import { buttonVariants } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link'
 import React from 'react'
 
 export default function layout({ children }: { children: React.ReactNode }) {
     return (
         <div className='relative flex flex-col items-center justify-center min-h-svh'>
+            <Link href='/' className={buttonVariants({
+                variant: 'outline',
+                className: 'absolute top-4 left-4'
+            })}>
+                <ArrowLeft size={4} /> Back
+            </Link>
             <div className='flex flex-col w-full max-w-sm gap-6'>
+                <Link href='/' className='flex items-center self-center gap-2 font-medium'>
+                    <Image src='/white_logo_lms3.png' alt='LMS Logo' width={80} height={80} />
+                    <span className='text-2xl font-extrabold'>LMS</span>
+                </Link>
                 {children}
+                <div className='text-xs text-center text-balance text-muted-foreground'>
+                    By clicking continue, you agree to our <span className='hover:text-primary hover:underline'>Terms of Service</span> {' '} and <span className='hover:text-primary hover:underline'>Privacy Policy</span>.
+                </div>
             </div>
         </div>
     );
